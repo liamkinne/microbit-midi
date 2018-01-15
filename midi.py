ee71e9b7563457294a2aa1a3977748a59dc63c52
liamkinne <kinneliam@gmail.com>
2018-01-15 22:09:17 +1100

    init

---
+++ midi.py
@@ -0,0 +1,20 @@
+from microbit import *
+
+class Midi:
+  def __init__(self, pin_tx, pin_rx):
+    self.pin_tx = pin_tx
+    self.pin_rx = pin_rx
+
+    self.commands = {
+      'note_off' : 0x80,
+      'note_on' : 0x90,
+      'aftertouch' : 0xA0,
+      'continuous_controller' : 0xB0,
+      'patch_change' : 0xC0,
+      'channel_pressure' : 0xD0,
+      'pitch_bend' : 0xE0,
+      'non_musical' : 0xF0,
+    }
+
+  def send_command(self, command, data):
+    #
