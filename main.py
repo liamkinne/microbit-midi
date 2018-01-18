260e034943d22bc5da649675d437a36c902ae024
liamkinne <kinneliam@gmail.com>
2018-01-18 21:13:35 +1100

    change test file

---
+++ main.py
@@ -0,0 +1,13 @@
+from microbit import *
+from midi import *
+
+m = Midi(pin0, pin1)
+
+notes = ["F4", "A4", "C4", "E4"] # F,A,C,E
+
+while True:
+	for n in notes:
+		m.note_on(n, 127)
+		sleep(500)
+		m.note_off(n, 127)
+		sleep(500)
